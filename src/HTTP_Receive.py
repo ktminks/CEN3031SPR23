74407b0eff94f049fb22c31b9f6cb9592c5d68ea
JRM128 <jmclaren@ufl.edu>
2023-03-09 19:22:21 -0500

    Update HTTP_Receive.py
    Added test function to the file

--- src/HTTP_Receive.py
+++ src/HTTP_Receive.py
@@ -21,7 +21,7 @@ def getall_recipes(name):
     recipes_array = table.find({}).toArray()
     return recipes_array
 
-'''# POST (Create) Method
+# POST (Create) Method
 @route('/', method = 'POST')
 def add_recipe():
     # Receive receipe and check for any errors
@@ -44,7 +44,7 @@ def add_recipe():
     except:
         abort(404, "Data not inserted into database")
 
-# Delete Method
+'''# Delete Method
 @route('/<name>', method = 'DELETE')
 def update_recipe(name):
     recipe = table.find_one({}, {'Name': name})
@@ -57,3 +57,17 @@ def update_recipe(name):
 
 # Run API on the localhost server at port 4200
 run(host = "localhost", port = 4200)
+
+import unittest
+class TestDB(unittest.TestCase):
+    def test_getAll(self):
+        #get all of the recipes in the database
+        temp = table.find({})
+        count = 0
+        #for each recipe, increase count by one
+        for recipe in temp:
+            count = count + 1
+        funct_test= getall_recipes("")
+        self.assertEqual(count, len(funct_test))
+
+unittest.main(argv = [''], verbosity = 2, exit = False)
