d9060b1a1e8d21dfe93acd0b621f8e6466901140
KT Minckler <katie.minckler@gmail.com>
2023-04-18 13:50:45 -0400

    run scripts

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   standalone: true,
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss']
+  styleUrls: ['./banner.component.scss'],
+  imports: [MatButtonModule]
 })
 export class BannerComponent {
 
